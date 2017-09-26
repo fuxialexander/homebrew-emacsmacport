61c479d8763173c468bf41a6c749c9c3555af539
fuxialexander <fuxialexander@users.noreply.github.com>
2017-09-26 15:36:31 +0800

    Update emacs-mac.rb

--- Formula/emacs-mac.rb
+++ Formula/emacs-mac.rb
@@ -1,9 +1,9 @@
 class EmacsMac < Formula
   desc "YAMAMOTO Mitsuharu's Mac port of GNU Emacs"
   homepage "https://www.gnu.org/software/emacs/"
-  url "https://bitbucket.org/mituharu/emacs-mac/get/emacs-25.3-mac-6.7.tar.bz2"
-  version "emacs-25.3-mac-6.7"
-  sha256 "0df71057f6ef378813b649b322b2bf62d14c8b33744d1838ec6353d387aff577"
+  url "https://bitbucket.org/mituharu/emacs-mac/get/emacs-25.3-mac-6.8.tar.bz2"
+  version "emacs-25.3-mac-6.8"
+  sha256 ""
 
   head "https://bitbucket.org/mituharu/emacs-mac.git", branch: "work"
 
